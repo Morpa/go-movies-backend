da97fbf11902278a86686e42fb375c7badc806de
Morpa <andremorpa@gmail.com>
2024-01-05 13:55:34 +0000

    add routing package

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -25,7 +25,7 @@ func main() {
 	log.Println("Starting application on port", port)
 
 	// start web server
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
